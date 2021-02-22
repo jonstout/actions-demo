24ed68b10b53859810a9391702e6b2e88d363282
Jonathan Stout <jonstout@indiana.edu>
2021-02-22 18:40:17 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+import unittest
+
+from jonstout.hello import hello_message
+
+
+class TestHello(unittest.TestCase):
+  def test_hello_message(self):
+    testdata = [
+      { 'name': "Jonathan", 'result': 'Hello Jonathan." }
+    ]
+    
+    for data in testdata:
+      self.assertEqual(hello_message(data['name']), data['result'])
+
+if __name__ == '__main__':
+  unittest.main()
